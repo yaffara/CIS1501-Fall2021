b1b31f8e8bad51efcd01a6c8a0bbcafdd3c196f0
Eric Charnesky <eric.charnesky@careevolution.com>
2021-09-07 16:33:02 -0400

    Chapter 1 examples

---
+++ Chapter1/main.py
@@ -0,0 +1,10 @@
+# this is a comment and is ignored by the python interpreter
+
+# floats are numeric values with decimal places
+# ints are numeric whole numbers - integers
+hourly_pay_rate = float(input('Enter your hourly pay rate: '))
+hours_worked_this_week = float(input('Enter your hours worked this week: '))
+pay_this_week = hourly_pay_rate * hours_worked_this_week
+print('your pay this week:', pay_this_week)
+
+print('test test')

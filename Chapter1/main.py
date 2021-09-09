c12b62a28c792940f45a838e602de0345b877a56
Eric Charnesky <eric.charnesky@careevolution.com>
2021-09-09 16:44:02 -0400

    updated examples

--- Chapter1/main.py
+++ Chapter1/main.py
@@ -1,3 +1,5 @@
+import math
+
 # this is a comment and is ignored by the python interpreter
 
 # floats are numeric values with decimal places
@@ -7,4 +9,22 @@ hours_worked_this_week = float(input('Enter your hours worked this week: '))
 pay_this_week = hourly_pay_rate * hours_worked_this_week
 print('your pay this week:', pay_this_week)
 
-print('test test')
+# int - Integer - whole numbers
+credits_this_semester = int(input('how many credits are you taking this semester?'))
+total_credits_required_for_degree = int(input('how many credits do you need to get your degree?'))
+credits_completed = int(input('How many credits do you already have completed?'))
+
+credits_remaining = total_credits_required_for_degree - credits_completed - credits_this_semester
+
+print('after this semester, you will need', credits_remaining, 'credits to complete your degree')
+
+number_of_semesters_to_go = credits_remaining / credits_this_semester
+
+# math.ceil( some_number_here ) - will round up to next integer
+print('if you take', credits_this_semester, 'credits',
+      'each semester, you have', math.ceil(number_of_semesters_to_go), 'semesters left until you graduate')
+
+
+name = input('what is your name?')
+print('hi', name)
+

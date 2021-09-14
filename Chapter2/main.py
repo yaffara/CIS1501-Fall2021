41398f7f0eda2140db9ddfdf7b6895bf37a1aaf6
Eric Charnesky <eric.charnesky@careevolution.com>
2021-09-14 16:14:00 -0400

    chapter 2 examples

--- Chapter2/main.py
+++ Chapter2/main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+import math
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print('happy tuesday')
+
+hourly_pay_rate = 15.5
+hours_worked = 40
+total_pay = hours_worked * hourly_pay_rate
+name = 'Eric'
+
+speed_of_light_approximate = 3e8
+
+print(speed_of_light_approximate, 'meters/second')
+# value is the default
+print('your pay this week', total_pay)
+
+print("type(total_pay): ", type(total_pay))
+print("id(total_pay): ", id(total_pay))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print('Pi to 5 decimal places: {:.5f}'.format(math.pi))
 
+# exponential operator, x^y
+print("3 to the 5th power is: ", 3**5)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# modulus is like divison, but gives the integer remainder
+print("13 % 5 == ", 13 % 5 )
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# PEMDAS - parentheses , exponents, multiplication, division, addition, subtraction
+print(' 10 - 5 + 10')
+
+print('what is 10 - 10 / 5 x 10 + 10')
+
+print(hourly_pay_rate)
+hourly_pay_rate = hourly_pay_rate + 1
+print(hourly_pay_rate)
+hourly_pay_rate += 1
+print(hourly_pay_rate)
+
+# can use an underscore for digit grouping
+one_billion = 1_000_000_000
+
+# // is for integer division
+print( '5/2 as an integer result == ', 5 // 2, 'remainder ', 5 % 2 )
+
+print('A', ord('A'))
+print('69 as a character', chr(69))
+print('82 as a character', chr(82))
+print('73 as a character', chr(73))
+print('67 as a character', chr(67))
+
+print('Tab: \t - newline: \n - single quote \'')
+print(r'Tab: \t - newline: \n - single quote \'')

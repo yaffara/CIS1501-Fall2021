9336c6b13aa5b0f06dbbb94d85ba558111ae5bb4
yaffara <90241887+yaffara@users.noreply.github.com>
2021-09-20 04:29:45 -0400

    Chp2

---
+++ Chp2.py
@@ -0,0 +1,36 @@
+import math
+
+print("happy tuesday")
+
+hourly_pay_rate = 15.5
+hours_worked = 40
+total_pay = hours_worked * hourly_pay_rate
+name = "yusif"
+
+speed_of_light_approximate = 3e8
+
+print(speed_of_light_approximate, "meters/seconds")
+print("your pay this week", total_pay)
+
+print("type(total_pay): ", type(total_pay))
+print("id(total_pay): ", id(total_pay))
+
+
+
+print('Pi to 5 decimal places: {:.5f}' .format(math.pi))
+
+
+print("3 to the 5th power is: ", 3**5)
+
+# gives integer remainder
+print("13 % 5 == " 13 % 5)
+
+hourly_pay_rate + hourly_pay_rate + 1
+print(hourly_pay_rate)
+hourly_pay_rate += 1
+print(hourly_pay_rate)
+
+one_billion = 1_000_000_000
+
+print("5/2 as an integer result")
+
